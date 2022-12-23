91d444a2b0cc7be51acab42dbddff073b0b34717
Vladimir <vovachempogir@mail.ru>
2022-12-23 11:27:14 +0700

    билет 3

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "разделитель";
+        int i = s.length() / 2;
+        System.out.println(s.substring(0,i));
+        System.out.println(s.substring(i));
+
+    }
+}
